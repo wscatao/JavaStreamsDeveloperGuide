3cdf360caa41f873e8af96bdcb99970fa784aec3
Willy <wscatao@gmail.com>
2022-04-17 12:28:05 -0300

    Exercicio aula 1.

--- src/test/java/lectures/Lecture1.java
+++ src/test/java/lectures/Lecture1.java
@@ -6,6 +6,7 @@ import beans.Person;
 
 import com.google.common.collect.ImmutableList;
 import java.io.IOException;
+import java.util.ArrayList;
 import java.util.List;
 import java.util.stream.Collectors;
 
@@ -20,7 +21,23 @@ public class Lecture1 {
   public void imperativeApproach() throws IOException {
     List<Person> people = MockData.getPeople();
     // 1. Find people aged less or equal 18
+
+    List<Person> only18orLessList = people.stream().filter(person -> person.getAge() <= 18).collect(Collectors.toList());
+
+    System.out.println(only18orLessList);
+
+
     // 2. Then change implementation to find first 10 people
+
+    int i = 10;
+    List<Person> firt10 = new ArrayList<>();
+
+    while (i > 0) {
+      firt10.add(only18orLessList.get(i));
+      i--;
+    }
+
+    System.out.println(firt10);
 
   }
 
