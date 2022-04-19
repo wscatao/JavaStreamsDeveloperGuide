d233567ec2963b8f2e711d3b078ccb27e64afd9d
Willy <wscatao@gmail.com>
2022-04-18 22:01:51 -0300

    Realizado filtro de forma imperativa e depois da forma declarativa com stream.

--- src/test/java/lectures/Lecture1.java
+++ src/test/java/lectures/Lecture1.java
@@ -1,49 +1,58 @@
 package lectures;
 
-import static org.assertj.core.api.Assertions.assertThat;
-
 import beans.Person;
-
 import com.google.common.collect.ImmutableList;
+import mockdata.MockData;
+import org.junit.Test;
+
 import java.io.IOException;
 import java.util.ArrayList;
 import java.util.List;
 import java.util.stream.Collectors;
 
-import mockdata.MockData;
-import org.assertj.core.util.Lists;
-import org.junit.Test;
-
 
 public class Lecture1 {
 
-  @Test
-  public void imperativeApproach() throws IOException {
-    List<Person> people = MockData.getPeople();
-    // 1. Find people aged less or equal 18
+    @Test
+    public void imperativeApproach() throws IOException {
+        List<Person> people = MockData.getPeople();
+        // 1. Find people aged less or equal 18
 
-    List<Person> only18orLessList = people.stream().filter(person -> person.getAge() <= 18).collect(Collectors.toList());
+        List<Person> youngPeople = new ArrayList<>();
 
-    System.out.println(only18orLessList);
+        for (Person person : people) {
+            if (person.getAge() <= 18) {
+                youngPeople.add(person);
+            }
+        }
 
+        System.out.println(youngPeople);
+        // 2. Then change implementation to find first 10 people
 
-    // 2. Then change implementation to find first 10 people
+        int limit = 10;
+        int start = 1;
+        List<Person> firt10 = new ArrayList<>();
 
-    int i = 10;
-    List<Person> firt10 = new ArrayList<>();
+        for (Person person : youngPeople) {
+            if (start <= limit) {
+                firt10.add(person);
+                start += 1;
+            }
+        }
 
-    while (i > 0) {
-      firt10.add(only18orLessList.get(i));
-      i--;
+        System.out.println(firt10.size());
     }
 
-    System.out.println(firt10);
+    @Test
+    public void declarativeApproachUsingStreams() throws Exception {
+        ImmutableList<Person> people = MockData.getPeople();
 
-  }
+        people.
+                stream()
+                .filter(person -> person.getAge() <= 18)
+                .limit(10)
+                .collect(Collectors.toList())
+                .forEach(System.out::println);
 
-  @Test
-  public void declarativeApproachUsingStreams() throws Exception {
-    ImmutableList<Person> people = MockData.getPeople();
-
-  }
+    }
 }
