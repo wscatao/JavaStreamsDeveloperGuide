1ac93b713af32a1d5d0993cf9fb2f97d24a06c4c
Willy <wscatao@gmail.com>
2022-04-22 20:38:24 -0300

    extraindo uma lista sem duplicatas com distinct ou set

--- src/test/java/lectures/Lecture4.java
+++ src/test/java/lectures/Lecture4.java
@@ -4,6 +4,8 @@ package lectures;
 import static org.assertj.core.api.Assertions.assertThat;
 
 import com.google.common.collect.ImmutableList;
+
+import java.util.HashSet;
 import java.util.List;
 import java.util.Set;
 import java.util.stream.Collectors;
@@ -15,11 +17,26 @@ public class Lecture4 {
   public void distinct() throws Exception {
     final List<Integer> numbers = ImmutableList.of(1, 1, 2, 2, 3, 3, 4, 4, 5, 5, 6, 6, 7, 7, 8, 8, 9, 9, 9, 9, 9);
 
+    List<Integer> distinctNumbers = numbers
+            .stream()
+            .distinct()
+            .collect(Collectors.toList());
+
+    assertThat(distinctNumbers).hasSize(9);
+
+    System.out.println(distinctNumbers);
+
   }
 
   @Test
   public void distinctWithSet() throws Exception {
     final List<Integer> numbers = ImmutableList.of(1, 1, 2, 2, 3, 3, 4, 4, 5, 5, 6, 6, 7, 7, 8, 8, 9, 9, 9, 9, 9);
+
+    Set<Integer> distinctNumbers = new HashSet<>(numbers);
+
+    assertThat(distinctNumbers).hasSize(9);
+
+    System.out.println(distinctNumbers);
 
   }
 }
