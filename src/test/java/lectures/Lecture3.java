d7ca58c27d5e7d115d807de91f98f4066b7658f4
Willy <wscatao@gmail.com>
2022-04-22 00:55:22 -0300

    Utilizando o .min e .max para comparar numeros em uma lista com o Comparator.

--- src/test/java/lectures/Lecture3.java
+++ src/test/java/lectures/Lecture3.java
@@ -1,10 +1,13 @@
 package lectures;
 
+import static org.assertj.core.api.Assertions.*;
 import static org.assertj.core.api.Assertions.assertThat;
 
 import com.google.common.collect.ImmutableList;
 import java.util.Comparator;
 import java.util.List;
+
+import org.assertj.core.api.Assertions;
 import org.junit.Test;
 
 public class Lecture3 {
@@ -13,11 +16,27 @@ public class Lecture3 {
   public void min() throws Exception {
     final List<Integer> numbers = ImmutableList.of(1, 2, 3, 100, 23, 93, 99);
 
+    Integer minNumber = numbers.
+            stream().
+            min(Comparator.naturalOrder()).
+            get();
+
+    assertThat(minNumber).isEqualTo(1);
+    System.out.println(minNumber);
+
   }
 
   @Test
   public void max() throws Exception {
     final List<Integer> numbers = ImmutableList.of(1, 2, 3, 100, 23, 93, 99);
+
+    Integer maxNumber = numbers.
+            stream().
+            max(Comparator.naturalOrder())
+            .get();
+
+    assertThat(maxNumber).isEqualTo(100);
+    System.out.println(maxNumber);
 
   }
 }
