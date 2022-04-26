b804a58669456e40cf6ec69e502468839a346409
Willy <wscatao@gmail.com>
2022-04-25 23:13:53 -0300

    Utilizando reduce para somar.

--- src/test/java/lectures/Lecture9.java
+++ src/test/java/lectures/Lecture9.java
@@ -1,9 +1,9 @@
 package lectures;
 
 
-import com.google.common.collect.Lists;
-import java.util.Arrays;
 import org.junit.Test;
+
+import java.util.Arrays;
 
 public class Lecture9 {
 
@@ -11,8 +11,15 @@ public class Lecture9 {
   public void reduce() throws Exception {
     Integer[] integers = {1, 2, 3, 4, 99, 100, 121, 1302, 199};
 
+    Integer sum1 = Arrays.stream(integers)
+            .reduce(0, (a, b) -> a + b);
+
+    System.out.println(sum1);
+
+    Integer sum2 = Arrays.stream(integers).reduce(0, Integer::sum);
+
+    System.out.println(sum2);
+
   }
-
-
 }
 
