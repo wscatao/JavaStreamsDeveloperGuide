d83cc8c7e91f4804f95c1a2453fe58168e3366e7
Willy <wscatao@gmail.com>
2022-04-25 23:08:47 -0300

    Utilizado grouping by e count para agrupamento de dados com stream

--- src/test/java/lectures/Lecture8.java
+++ src/test/java/lectures/Lecture8.java
@@ -2,20 +2,30 @@ package lectures;
 
 
 import beans.Car;
+import com.google.common.collect.ImmutableList;
+import mockdata.MockData;
+import org.assertj.core.util.Lists;
+import org.junit.Test;
+
 import java.util.ArrayList;
 import java.util.List;
 import java.util.Map;
 import java.util.function.Function;
-import java.util.stream.Collector;
 import java.util.stream.Collectors;
-import mockdata.MockData;
-import org.assertj.core.util.Lists;
-import org.junit.Test;
 
 public class Lecture8 {
 
   @Test
   public void simpleGrouping() throws Exception {
+
+    ImmutableList<Car> cars = MockData.getCars();
+
+    Map<String, List<Car>> carsGroupedByMake = cars.stream().collect(Collectors.groupingBy(Car::getMake));
+
+    carsGroupedByMake.forEach((make, makeCars) -> {
+      System.out.println(make);
+      makeCars.forEach(System.out::println);
+    });
 
   }
 
@@ -34,7 +44,9 @@ public class Lecture8 {
             "Alex"
         );
 
+    Map<String, Long> countEachNameAppear = names.stream().collect(Collectors.groupingBy(Function.identity(), Collectors.counting()));
 
+    countEachNameAppear.forEach((name, times) -> System.out.println(name + " > " + times));
   }
 
 }
