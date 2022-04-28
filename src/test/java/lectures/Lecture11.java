c97979ee189de1ed914ee896021c683ca39b10d0
Willy <wscatao@gmail.com>
2022-04-27 23:23:32 -0300

    Utilizando o joining para concatenar strings.

--- src/test/java/lectures/Lecture11.java
+++ src/test/java/lectures/Lecture11.java
@@ -1,22 +1,34 @@
 package lectures;
 
 import com.google.common.collect.ImmutableList;
+
 import java.util.List;
+import java.util.Optional;
 import java.util.stream.Collectors;
+
 import mockdata.MockData;
 import org.junit.Test;
 
 public class Lecture11 {
 
-  @Test
-  public void joiningStrings() throws Exception {
-    List<String> names = ImmutableList.of("anna", "john", "marcos", "helena", "yasmin");
+    @Test
+    public void joiningStrings() throws Exception {
+        List<String> names = ImmutableList.of("anna", "john", "marcos", "helena", "yasmin");
 
-  }
+        String s = names.stream().reduce(String::concat).get();
 
-  @Test
-  public void joiningStringsWithStream() throws Exception {
-    List<String> names = ImmutableList.of("anna", "john", "marcos", "helena", "yasmin");
+        System.out.println(s);
+    }
 
-  }
+    @Test
+    public void joiningStringsWithStream() throws Exception {
+        List<String> names = ImmutableList.of("anna", "john", "marcos", "helena", "yasmin");
+
+        String string = names
+                .stream()
+                .collect(Collectors.joining(", "));
+
+        System.out.println(string);
+
+    }
 }
