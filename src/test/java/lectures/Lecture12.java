9648a56c488aeeee839105d85c34a1a8b7926930
Willy <wscatao@gmail.com>
2022-04-27 23:29:20 -0300

    Entendendo o collect.

--- src/test/java/lectures/Lecture12.java
+++ src/test/java/lectures/Lecture12.java
@@ -4,6 +4,8 @@ import beans.Person;
 import java.util.ArrayList;
 import java.util.List;
 import java.util.stream.Collectors;
+
+import com.google.common.collect.Lists;
 import mockdata.MockData;
 import org.junit.Test;
 
@@ -13,7 +15,12 @@ public class Lecture12 {
     List<String> emails = MockData.getPeople()
         .stream()
         .map(Person::getEmail)
-        .collect(Collectors.toList());
+        .collect(
+                () -> Lists.newArrayList(),
+                (list, element) -> list.add(element),
+                (list1, list2) -> list1.addAll(list2));
+
+//                Collectors.toList()
 
     emails.forEach(System.out::println);
   }
